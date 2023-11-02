75b6c3fdd717576faeb3bf56e0db5df5798c916a
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-02 15:53:09 +0000

    Update Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const { db } = require("../db/connection");
 const { Sequelize, DataTypes } = require("sequelize");
 
-let comment = db.define("comment", {
+let comment = db.define("comment ", {
     body: DataTypes.STRING,
     createdAt: DataTypes.STRING
 })
